56927307ba048b0e550466f0a882694ee726dee6
Bhavesh <bhavesh.b.odedra@gmail.com>
2014-06-26 11:23:47 +0530

    FIX in MO, branch add when procurement is run.

--- company_extended/company_extended.py
+++ company_extended/company_extended.py
@@ -20,6 +20,8 @@
 ##############################################################################
 
 import time
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
 from openerp.osv import osv, fields
 from openerp.tools.translate import _
 from openerp import netsvc
@@ -143,6 +145,45 @@ class procurement_order(osv.Model):
     def create_procurement_purchase_order(self, cr, uid, procurement, po_vals, line_vals, context=None):
         po_vals.update({'order_line': [(0,0,line_vals)], 'branch_id': procurement.branch_id.id})
         return self.pool.get('purchase.order').create(cr, uid, po_vals, context=context)
+        
+    def make_mo(self, cr, uid, ids, context=None):
+        """ Make Manufacturing(production) order from procurement
+        @return: New created Production Orders procurement wise 
+        """
+        res = {}
+        company = self.pool.get('res.users').browse(cr, uid, uid, context).company_id
+        production_obj = self.pool.get('mrp.production')
+        move_obj = self.pool.get('stock.move')
+        wf_service = netsvc.LocalService("workflow")
+        procurement_obj = self.pool.get('procurement.order')
+        for procurement in procurement_obj.browse(cr, uid, ids, context=context):
+            res_id = procurement.move_id.id
+            newdate = datetime.strptime(procurement.date_planned, '%Y-%m-%d %H:%M:%S') - relativedelta(days=procurement.product_id.produce_delay or 0.0)
+            newdate = newdate - relativedelta(days=company.manufacturing_lead)
+            produce_id = production_obj.create(cr, uid, {
+                'origin': procurement.origin,
+                'product_id': procurement.product_id.id,
+                'product_qty': procurement.product_qty,
+                'product_uom': procurement.product_uom.id,
+                'product_uos_qty': procurement.product_uos and procurement.product_uos_qty or False,
+                'product_uos': procurement.product_uos and procurement.product_uos.id or False,
+                'location_src_id': procurement.location_id.id,
+                'location_dest_id': procurement.location_id.id,
+                'bom_id': procurement.bom_id and procurement.bom_id.id or False,
+                'date_planned': newdate.strftime('%Y-%m-%d %H:%M:%S'),
+                'move_prod_id': res_id,
+                'company_id': procurement.company_id.id,
+                'branch_id': procurement.branch_id.id or False,
+            })
+            
+            res[procurement.id] = produce_id
+            self.write(cr, uid, [procurement.id], {'state': 'running', 'production_id': produce_id})   
+            bom_result = production_obj.action_compute(cr, uid,
+                    [produce_id], properties=[x.id for x in procurement.property_ids])
+            wf_service.trg_validate(uid, 'mrp.production', produce_id, 'button_confirm', cr)
+        self.production_order_create_note(cr, uid, ids, context=context)
+        return res
+
         
 class mrp_production(osv.Model):
     _inherit = 'mrp.production'
